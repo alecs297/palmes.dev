5b373ab3e6765d3052e13233acaafc41ae467974
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-08-13 20:10:02 +0200

    new "engine" for movement

--- src/jsx/Duck.js
+++ src/jsx/Duck.js
@@ -5,6 +5,102 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Suspense, useEffect } from 'react'
 import { AnimationMixer } from "three"
 
+// convert distance to animation frames
+const ms_per_percentage = 0.15;
+// inactivity timeout before switching to idle animation
+const move_timeout_ms = 500
+
+// frame is scrolled percentage
+const movements = [
+    {
+        frame: 1,
+        position: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        rotation: {
+            x: 0,
+            y: 0.8,
+            z: 0
+        }
+    },
+    {
+        frame: 25,
+        position: {
+            x: 0.75,
+            y: 0,
+            z: 0
+        },
+        rotation: {
+            x: 0,
+            y: Math.PI / 2,
+            z: 0
+        }
+    },
+    {
+        frame: 40,
+        position: {
+            x: 1,
+            y: -0.5,
+            z: 0
+        },
+        rotation: {
+            x: 0,
+            y: 0,
+            z: 0
+        }
+    },
+    {
+        frame: 60,
+        position: {
+            x: 1,
+            y: -0.8,
+            z: 0
+        },
+        rotation: {
+            x: 0,
+            y: -Math.PI / 2,
+            z: 0
+        }
+    },
+    {
+        frame: 99,
+        position: {
+            x: 0.5,
+            y: 0,
+            z: 1
+        },
+        rotation: {
+            x: 0,
+            y: -0.9,
+            z: 0
+        }
+    },
+]
+
+function updatePositions(object, start, end, scrollY, reverse=false, just_reversed=0) {
+
+    object.position.x = getPath(start.position.x, end.position.x, scrollY, start.frame, end.frame);
+    object.position.y = getPath(start.position.y, end.position.y, scrollY, start.frame, end.frame);
+    object.position.z = getPath(start.position.z, end.position.z, scrollY, start.frame, end.frame);
+    object.rotation.x = getPath(start.rotation.x, end.rotation.x, scrollY, start.frame, end.frame) % (2 * Math.PI);
+    object.rotation.z = getPath(start.rotation.z, end.rotation.z, scrollY, start.frame, end.frame) % (2 * Math.PI);
+
+    if (reverse) {
+        object.rotation.y = getPath((start.rotation.y + Math.PI), (end.rotation.y + (Math.PI * !just_reversed)), scrollY, start.frame, end.frame) % (2 * Math.PI);
+    } else {
+        object.rotation.y = getPath(start.rotation.y, end.rotation.y, scrollY, start.frame, end.frame) % (2 * Math.PI);
+    }
+
+}
+
+function getPath(start, end, scroll, frame_start, frame_end) {
+    const percentage = (((scroll * 100) - frame_start) / (frame_end - frame_start));
+    let r = (end - start) * percentage + start
+    return isFinite(r) ? r : start
+}
+
 function Duck({origin=null}) {
 
     let state = null;
@@ -19,21 +115,7 @@ function Duck({origin=null}) {
     let animation = null;
     let last_frame_scroll = 0;
     let last_time_scroll = Date.now();
-
-    const move_timeout_ms = 100
-    const start_pos_x = 0
-    const end_pos_x = 10
-
-    const start_rot_y = -Math.PI/2
-    const end_rot_y = start_rot_y + Math.PI
-    const middle_rot_y = -0.8
-
-    // convert distance to animation frames
-    const ms_per_percentage = 0.15;
-    // percentage of scroll allowed for turning
-    const percentage_for_turn = 0.1
-    // percentage of scroll allowed for moving
-    const percentage_for_move = 0.7
+    let last_frame_reverse = false;
 
     function getScrollPercent() {
         let res = (-1)*(origin.current.getBoundingClientRect().top + window.scrollY) / document.body.getBoundingClientRect().height
@@ -52,42 +134,54 @@ function Duck({origin=null}) {
         }
     }
 
-    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useEffect(() => updatePositions(duck.current, movements[0], movements[0], 0), [duck])
+
     useFrame((state, delta) => {
 
         const scrollY = getScrollPercent();
-        const moving = last_frame_scroll !== scrollY;
-        const is_good_way = (duck.current.rotation.y === (scrollY - last_frame_scroll < 0 ? start_rot_y : end_rot_y))
+        const moving = (scrollY === last_frame_scroll) ? 0 : (scrollY > last_frame_scroll ? 1 : -1);
 
+        let start = movements[0];
+        let end = movements[1];
+
+        for (let i = 0; i < movements.length && movements[i].frame <= (scrollY * 100); i++) {
+            const frame = movements[i];
+            start = frame;
+            end = (i < movements.length - 1) ? movements[i + 1] : frame;
+        }
+
+        const reverse = moving === -1 && start.frame !== end.frame;
+        
         if (moving) {
-            switchAnimation(1)
-            if (!is_good_way) {
-                let rotation = duck.current.rotation.y + ( end_rot_y - start_rot_y ) * ((scrollY - last_frame_scroll) / percentage_for_turn);
-                duck.current.rotation.y = Math.min(Math.max(start_rot_y, rotation), end_rot_y)
 
-            }
-            let position_x = (end_pos_x - start_pos_x) * ((scrollY) / (percentage_for_move))
-            duck.current.position.x = position_x
-            
+            switchAnimation(1)
+
+            updatePositions(duck.current, start, end, scrollY, reverse, (last_frame_reverse === end.frame))
 
             mixer.update(Math.abs(scrollY - last_frame_scroll) / ms_per_percentage)
             last_time_scroll = Date.now()
 
-        } else {
-            if (Date.now() - last_time_scroll > move_timeout_ms) {
-                switchAnimation(0)
-                mixer.update(delta)
-            } else if (scrollY === 0 || scrollY === 1) {
-                if (duck.current.rotation.y < middle_rot_y) duck.current.rotation.y++
+        } else if (Date.now() - last_time_scroll > move_timeout_ms) {
+
+            if (scrollY === 0 && duck.current.rotation.y !== start.rotation.y) {
+                let begin = (duck.current.rotation.y) % (2 * Math.PI)
+                let target = (start.rotation.y) % (2 * Math.PI)
+                let rotation = Math.PI / 10
+
+                if (Math.abs(target - begin) < rotation) {
+                    duck.current.rotation.y = start.rotation.y
+                } else {
+                    duck.current.rotation.y = (duck.current.rotation.y - rotation) % (Math.PI * 2);
+                }
+
             }
+            switchAnimation(0)
+            mixer.update(delta);
         }
 
-        last_frame_scroll = scrollY
+        last_frame_scroll = scrollY;
+        last_frame_reverse = moving === 1 ? false : (last_frame_reverse || end.frame)
     })
-
-    useEffect(() => {
-        duck.current.rotation.y = middle_rot_y;
-    }, [duck, start_rot_y])
 
     // Return the view, these are regular Threejs elements expressed in JSX
     return (
