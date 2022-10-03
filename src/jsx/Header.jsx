3cacb1a017b50c13d7622493d28d97658f4f173a
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-03 10:05:35 +0200

    moved scroll indicator near name

--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -22,8 +22,10 @@ function Header() {
     
     return (
         <div className={"w-3/4 max-w-6xl text-center mx-auto p-5 h-6 flex items-center place-content-between sticky bottom-48 rounded-lg shadow-inner shadow-white-100 bg-black border-2 border-gray-200 z-90"}>
-            <h1 className="w-1/6">palmes.dev</h1>
-            <Typewritter className={"hidden md:inline"} textClassName={"text-sm text-stone-400"} cursorClassName={"text-stone-600"} options={{delete: false}}>scroll to get started</Typewritter>
+            <div>
+                <h1 className="w-1/6 md:inline">palmes.dev</h1>
+                <Typewritter className={"hidden md:inline"} textClassName={"text-sm text-stone-400"} cursorClassName={"text-stone-600"} options={{delete: false}}> | scroll to get started</Typewritter>
+            </div>
             <div className="w-1/6">
                 <ul className="bg-black border-y border-gray-800 absolute left-0 md:left-auto -top-10 md:w-1/6 w-full float-right -translate-y-full" hidden={!open}>
                     <li className="underline underline-offset-8 m-2" >Quick Links</li>
