49197ed32014ded4b5f1e070f2732047c7a2e8d0
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-13 18:46:54 +0200

    containered body

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -2,20 +2,26 @@ import Header from "./Header";
 import ThreeView from "./ThreeView";
 import NoContent from "./NoContent";
 import Bio from './Bio';
+import TechList from "./Techs";
 
 function App() {
 
     return (
         <div className="bg-black p-0 m-0 h-screen w-screen overflow-x-hidden text-white">
-            <div id="anchor-start"/>
-            <ThreeView/>
-            <NoContent/>
-            <Header/>
-            <NoContent size="1/4"/>
-            <Bio/>
-            <NoContent/>
-            <NoContent className='sticky'/>
-            <div id="anchor-end"/>
+            <div>
+                <div id="anchor-start"/>
+                <ThreeView/>
+                <NoContent/>
+                <Header/>
+                <NoContent size="1/4"/>
+                <Bio/>
+                <NoContent/>
+                <NoContent className='sticky'/>
+                <div id="anchor-end"/>
+            </div>
+            <div className="z-10">
+                <TechList/>
+            </div>
         </div>
     );
 }
