ec55f63991237702e736abe92c1370a0a698258a
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-13 22:50:16 +0200

    better camera angle

--- src/jsx/ThreeView.jsx
+++ src/jsx/ThreeView.jsx
@@ -9,14 +9,14 @@ function ThreeView() {
         <>
             <div className='h-screen w-full fixed z-0 pointer-events-none'>
                 <Canvas className="h-screen" ref={canvas} camera={{
-                    rotation: [-0.5, 0, 0],
+                    rotation: [-Math.PI/12, 0, 0],
                     position: [0, 2, 4],
                     type: "OrthographicCamera"
                 }}>
                     <ambientLight intensity={0.5}/>
-                    <pointLight shadow={0.5} intensity={2} position={[10, 0, 5]} />
-                    <gridHelper rotation={[0, Math.PI/4, 0]} args={[100, 100]} />
-                    <fog attach="fog" args={['#000', 0, 33]} />
+                    <pointLight shadow={0} intensity={2} position={[10, 0, 5]} />
+                    <gridHelper rotation={[0, Math.PI/4, 0]} args={[100, 100, "#444", "#444"]} />
+                    <fog attach="fog" args={['#000', 0, 20]} />
                     <Duck canvas={canvas}/>
                 </Canvas>
             </div>
