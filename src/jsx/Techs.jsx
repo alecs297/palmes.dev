061d12998f1dcbbfa197001da72a27be0eabb29b
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-13 22:50:25 +0200

    descriptions

--- src/jsx/Techs.jsx
+++ src/jsx/Techs.jsx
@@ -1,6 +1,11 @@
 const techs = [
     [
         {
+            name: "C",
+            logo: "c-plain",
+            url: "https://en.wikipedia.org/wiki/C_(programming_language)"
+        },
+        {
             name: "Node.js",
             logo: "nodejs-plain",
             url: "https://nodejs.org/"
@@ -9,11 +14,6 @@ const techs = [
             name: "PHP",
             logo: "php-plain",
             url: "https://www.php.net/"
-        },
-        {
-            name: "C",
-            logo: "c-plain",
-            url: "https://en.wikipedia.org/wiki/C_(programming_language)"
         },
         {
             name: "Python",
@@ -33,7 +33,7 @@ const techs = [
         {
             name: "CSS3",
             logo: "css3-plain",
-            url: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3"
+            url: "https://developer.mozilla.org/en-US/docs/Web/CSS"
         },
         {
             name: "JavaScript",
@@ -56,14 +56,14 @@ const techs = [
             name: "MySQL",
             logo: "mysql-plain",
             url: "https://www.mysql.com/"
-        },
-        {
-            name: "SQLAlchemy",
-            logo: "sqlalchemy-plain",
-            url: "https://www.sqlalchemy.org/"
-        },
+        }
     ],
     [
+        {
+            name: "Linux",
+            logo: "linux-plain",
+            url: "https://www.linux.org/"
+        },
         {
             name: "Nginx",
             logo: "nginx-original",
@@ -85,6 +85,30 @@ const techs = [
             name: "Docker",
             logo: "docker-plain",
             url: "https://www.docker.com/"
+        }
+    ],
+    [
+        {
+            name: "Selenium",
+            logo: "selenium-plain",
+            url: "https://www.selenium.dev/"
+        },
+        {
+            name: "Flask",
+            logo: "flask-original",
+            url: "https://flask.palletsprojects.com/en/2.0.x/"
+        },
+        {
+            name: "SQLAlchemy",
+            logo: "sqlalchemy-plain",
+            url: "https://www.sqlalchemy.org/"
+        }
+    ],
+    [
+        {
+            name: "Spring",
+            logo: "spring-plain",
+            url: "https://spring.io/"
         }
     ],
     [
@@ -127,26 +151,56 @@ const techs = [
             name: "Tailwind",
             logo: "tailwindcss-plain",
             url: "https://tailwindcss.com/"
-        },
+        }
     ]
     
 ];
 
+function TechDescription() {
+    return (
+        <div className="text-md tracking-tight md:text-xl p-3 bg-stone-1000 w-full xl:w-3/4 space-y-3">
+            <p>Some HR guy on Linkedin once said : <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500">languages and frameworks don't matter.</span></p>
 
+            <p>While his post is certainly inspired by a motivational quote he scrolled past on Facebook, <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500"><br />he is right</span>.</p>
+
+            <p>Software engineering is not bound to a specific tool.</p>
+            
+            <p><span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500">However</span>, mastering the right <span className="line-through decoration-4 decoration-red-700">deprecated JavaScript</span> framework for the job does save a lot of time.</p>
+
+            <p>I'm <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500">always learning new things</span>, but these are some of my <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500">favorite</span> bricks I use for building stuff.</p>
+
+            <p>Feel free to check out my
+                <span onClick={() => {window.open(
+                    "https://github.com/alecs297",
+                    "_blank"
+                )}} className="font-bold text-xl mx-1 cursor-pointer underline">
+                    <i className="devicon-github-original"></i> GitHub
+                </span> for some of my projects.
+            </p>
+        </div>
+    );
+}
+
+function TechTitle() {
+    return (
+        <h1 className="font-extrabold text-4xl md:text-6xl p-3 bg-stone-900 w-fit -translate-x-3">Tech I <span className="text-pink-600 text-5xl md:text-7xl font-black tracking-tighter">&lt;3</span> to work with</h1>
+    );
+}
 
 function TechList() {
     return (
         <div className="w-screen md:w-1/2 px-3 md:px-0 md:mx-3 h-screen float-right sticky">
             <div className="mx-3">
-                <h1 className="font-extrabold text-4xl md:text-6xl p-3 bg-stone-900 w-fit">Tech I <span className="text-pink-500 font-mono tracking-tighter font-black">&lt;3</span> and work with.</h1>
-                <div className="flex-wrap gap-2 md:gap-2 justify-center mx-auto w-full font-mono">
+                <TechTitle/>
+                <TechDescription     />
+                <div className="grid grid-cols-7 gap-2 px-12 xl:gap-8 content-evenly mx-auto w-full xl:w-3/4 xl:mx-0 font-mono bg-stone-1000">
                     {
                         techs.reduce((a, b) => a.concat(b), []).map((tech, index) => {
                             return (
-                                <div key={"tech-" + index} className="w-fit inline-block m-3">
+                                <div key={"tech-" + index} className="w-fit inline-block my-1">
                                     <a target="_blank" rel="noreferrer noopener" href={tech.url}>
-                                        <i className={`devicon-${tech.logo} text-4xl md:text-7xl`}></i>
-                                        <p className="hidden  md:block text-center text-xs">{tech.name}</p>
+                                        <i className={`devicon-${tech.logo} text-4xl xl:text-7xl`}></i>
+                                        <p className="hidden xl:block text-center text-xs">{tech.name}</p>
                                     </a>
                                 </div>
                             )
@@ -158,4 +212,4 @@ function TechList() {
     )
 }
 
-export default TechList;
+    export default TechList;
