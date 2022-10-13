e15cb162d1abc31f8341b252cd4931998d043183
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-13 22:50:05 +0200

    used new blacks

--- src/jsx/Bio.jsx
+++ src/jsx/Bio.jsx
@@ -17,7 +17,7 @@ function Bio() {
         <div className="w-full md:w-1/2 h-screen mx-4 my-16 md:m-0 md:grid grid-cols-1 sticky top-0 md:place-content-evenly z-10">
             <div className="place-self-center px-3">
                 <h2 className="text-5xl md:text-7xl font-extrabold py-3 pb-5 bg-stone-900 w-fit">Hello world.</h2>
-                <div className="md:text-3xl font-bold bg-stone-900 px-3 mx-3">
+                <div className="md:text-3xl font-bold bg-stone-1000 p-3 translate-x-3 -translate-y-3">
                     <p>
                         My name is
                         <span className="text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-cyan-500"> Alex, </span>
