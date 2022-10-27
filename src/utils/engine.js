a8f6cb0f1fd7b8b3721a1a97ac62ee0263dd3e7d
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-10-27 22:30:08 +0200

    fixed the weird modulos

--- src/utils/engine.js
+++ src/utils/engine.js
@@ -3,13 +3,13 @@ export function updatePositions(object, start, end, scrollY, reverse=false, just
     object.position.x = getPath(start.position.x, end.position.x, scrollY, start.frame, end.frame);
     object.position.y = getPath(start.position.y, end.position.y, scrollY, start.frame, end.frame);
     object.position.z = getPath(start.position.z, end.position.z, scrollY, start.frame, end.frame);
-    object.rotation.x = getPath(start.rotation.x, end.rotation.x, scrollY, start.frame, end.frame) % (2 * Math.PI);
-    object.rotation.z = getPath(start.rotation.z, end.rotation.z, scrollY, start.frame, end.frame) % (2 * Math.PI);
+    object.rotation.x = getPath(start.rotation.x, end.rotation.x, scrollY, start.frame, end.frame);
+    object.rotation.z = getPath(start.rotation.z, end.rotation.z, scrollY, start.frame, end.frame);
 
     if (reverse) {
-        object.rotation.y = getPath((start.rotation.y + Math.PI), (end.rotation.y + (Math.PI * !just_reversed)), scrollY, start.frame, end.frame) % (2 * Math.PI);
+        object.rotation.y = getPath((start.rotation.y + Math.PI), (end.rotation.y + (Math.PI * !just_reversed)), scrollY, start.frame, end.frame);
     } else {
-        object.rotation.y = getPath(start.rotation.y, end.rotation.y, scrollY, start.frame, end.frame) % (2 * Math.PI);
+        object.rotation.y = getPath(start.rotation.y, end.rotation.y, scrollY, start.frame, end.frame);
     }
 
 }
