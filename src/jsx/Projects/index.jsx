02e7c40093236fba272f1f1061d2d65632c0822d
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-01-04 11:58:26 +0100

    beauty

--- src/jsx/Projects/index.jsx
+++ src/jsx/Projects/index.jsx
@@ -10,14 +10,14 @@ function ProjectsContainer() {
     return (
         <div className="w-full lg:w-3/4 clear-both mx-auto h-min-[75vh]">
             <div className="w-1/12 hidden lg:inline-block float-left">
-                <h1 className="text-content-greyed vertical-lr h-full text-4xl text-center uppercase tracking-wider -scale-100 font-black">
+                <h1 className="select-none text-content-greyed vertical-lr h-full text-4xl text-center uppercase tracking-wider -scale-100 font-black">
                     {"/** Work in perpetual progress */"}
                 </h1>
             </div>
             <div className="w-full h-full lg:w-11/12 inline-block">
                 <ul className="flex flex-wrap space-x-4 space-y-2">
                     <li className="w-full lg:w-auto text-5xl lg:text-3xl tracking-tight px-2 py-2 font-bold select-none block my-8 lg:my-0 lg:inline">
-                        Projects' Showcase
+                        Projects
                     </li>
                     {
                         categories.map((category, index) => {
@@ -33,7 +33,7 @@ function ProjectsContainer() {
                     }
                 </ul>
                 <div className="w-full h-full grid border-t-4 border-content-default">
-                    <span className="px-2 py-2 min-h-[10vh]">
+                    <span className="px-2 py-2 min-h-[10vh] whitespace-pre-line">
                         <Typewritter options={{
                             writeMs: 1,
                             loop: false
@@ -41,7 +41,7 @@ function ProjectsContainer() {
                             {selectedCategory.description}
                         </Typewritter>
                     </span>
-                    <div className="flex flex-wrap w-full justify-center gap-4 p-2">
+                    <div className="flex flex-wrap w-full justify-center gap-4 p-2 my-4">
                         {
                             selectedCategory.projects.map((project, index) => {
                                 return <FeaturedProject {...project} key={"project-" + index}/>
@@ -55,33 +55,33 @@ function ProjectsContainer() {
     )
 }
 
-function FeaturedProject({ title, description, repo, website, image }) {
+function FeaturedProject({ title, description, repo, website }) {
     return (
-        <div className="bg-background-accent w-max rounded-lg px-2 py-2 w-full h-full">
-            <h1 className="select-none text-content-primary font-bold text-2xl">{title}</h1>
-            {
-                repo && (
-                    <a className="inline" href={repo} target="_blank" rel="noreferrer">
-                        <i className="devicon-github-original mr-2"></i>
-                        Open source
-                    </a>
-                )
-            }
-            {
-                website && (
-                    <div className="block w-full h-full">
-                        <a className="inline" href={website} target="_blank" rel="noreferrer">
-                            <span className="h-4 w-4 inline-flex mr-2 align-baseline">
-                                <span className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-green-500"></span>
-                                <span className="inline-flex rounded-full h-4 w-4 bg-green-500"></span>
-                            </span>
-                            Live demo
+        <div className="bg-background-accent-darker w-max rounded-lg h-max">
+            <div className="bg-background-accent w-max px-4 py-4 -translate-x-3 -translate-y-3">
+                <h1 className="select-none text-content-primary font-bold text-2xl">{title}</h1>
+                {
+                    repo && (
+                        <a className="inline" href={repo} target="_blank" rel="noreferrer">
+                            <i className="devicon-github-original mr-2"></i>
+                            Open source
                         </a>
-                    </div>
-                    
-                    
-                )
-            }
+                    )
+                }
+                {
+                    website && (
+                        <div className="block">
+                            <a className="inline text-content-links" href={website} target="_blank" rel="noreferrer">
+                                {website}
+                            </a>
+                        </div>
+                    )
+                }
+            </div>
+            
+            <span className="block w-max h-max p-4">
+                {description}
+            </span>
         </div>
     )
 }
