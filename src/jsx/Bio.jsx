b7a5aa80b27f3d1ace939f4a4e2a7a33cbcac478
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-09 20:26:26 +0100

    added email copy feature

--- src/jsx/Bio.jsx
+++ src/jsx/Bio.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Macbook, FakeMacbook} from "./Macbook";
 import Typewritter from "./Typewritter";
 
 function Bio() {
+
+    const email = "alex@palmes.dev";
 
     const stuff = [
         "doing freelance.",
@@ -17,6 +19,17 @@ function Bio() {
     ]
 
     const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+    const [copied, setCopied] = useState(false);
+
+    useEffect(() => {
+        if (copied) {
+            navigator.clipboard.writeText(email);
+            setTimeout(() => {
+                setCopied(false);
+            }, 1000);
+        }
+    }, [copied]);
     
     return (
         <div className="min-h-screen py-4 w-full flex flex-wrap">
@@ -29,7 +42,13 @@ function Bio() {
                 </div>
             </div>
             <div className="w-full order-1 lg:order-2 px-2 lg:inline-block lg:w-3/5 lg:float-right my-auto z-10">
-                <h2 className="text-5xl md:text-7xl font-extrabold px-2 py-3 pb-5 bg-background-accent w-fit translate-y-2 -translate-x-3"><code className="text-content-secondary">$</code> whoami</h2>
+                <div className="text-2xl md:text-4xl font-extrabold px-2 py-3 pb-5 lg:pb-3 bg-background-accent w-fit translate-y-2 -translate-x-3">
+                    {
+                     copied 
+                     ? <span className="tracking-tight text-content-greyed">Copied to clipboard</span>
+                     : <span onClick={() => setCopied(true)} placeholder="Click to select" className="text-content-primary-accent hover:text-content-secondary cursor-pointer">alex<span className="text-content-default">@</span>palmes.dev</span>
+                    }
+                </div>
                 <div className="md:text-2xl w-full lg:w-3/4 font-base bg-background-accent-darker p-3 tracking-tight space-y-2">
                     <p className="w-full">
                         I am an engineering student in
